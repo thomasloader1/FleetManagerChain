5fc9b039046702f296d2b81357a1d4ef5a3ed09f
admin <lucabarrios@alumno.unla.edu.ar>
2024-09-14 17:54:16 -0300

    cache proyect

--- FleetManagerChain.Domain/obj/Debug/net8.0/FleetManagerChain.Domain.AssemblyInfo.cs
+++ FleetManagerChain.Domain/obj/Debug/net8.0/FleetManagerChain.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FleetManagerChain.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4fa7e10885e126f83fab93c047bb5a01c9c37be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94b630e84da357e2228eac9ac2964767cc732ba5")]
 [assembly: System.Reflection.AssemblyProductAttribute("FleetManagerChain.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FleetManagerChain.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
