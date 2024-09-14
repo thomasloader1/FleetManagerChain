bf9e507fd523878d5c026af4dfbf4c3f01b3d4df
Gomez Tomas Gonzalo <talk.gtg@gmail.com>
2024-09-14 11:55:05 -0300

    Inicio del proyecto

---
+++ FleetManagerChain.Domain/Order.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace FleetManagerChain.Domain
+{
+    public class Order
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Type { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Code { get; set; }
+
+        public int? Size { get; set; }
+
+        public int? Weight { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public double? Distance { get; set; }
+
+        public double? DistanceWithTraffic { get; set; }
+
+        public double? TimeWithTraffic { get; set; }
+
+        [Required]
+        public State State { get; set; }  // El enum "Estado" se define abajo
+
+        [ForeignKey("TravelId")]
+        public Travel Travel { get; set; }    // Relación Many-to-One con Viaje
+
+        [ForeignKey("ZoneId")]
+        public Zone Zone { get; set; }   // Relación Many-to-One con Zona
+
+    }
+}
